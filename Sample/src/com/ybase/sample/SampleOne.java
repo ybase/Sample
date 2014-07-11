7d655ac62e419b1f7844b8a27d0c7cddcaf96514
yangxianbing126@126.com <yangxianbing126@126.com>
2014-07-11 15:17:11 +0800

    4

--- Sample/src/com/ybase/sample/SampleOne.java
+++ Sample/src/com/ybase/sample/SampleOne.java
@@ -3,7 +3,7 @@ package com.ybase.sample;
 public class SampleOne {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World! One");
+		System.out.println("Hello World!");
 	}
 
 }
--- Sample/src/com/ybase/sample/SampleTwo.java
+++
@@ -1,9 +0,0 @@
-package com.ybase.sample;
-
-public class SampleTwo {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World! Two");
-	}
-
-}
